478b634756617c379ac3be163a2929f745cbdcfc
Kevin Edwards <kevin.edwards@octanner.com>
2017-04-10 21:01:10 -0600

    the plan -- part 1

--- parse_data.hs
+++ parse_data.hs
@@ -11,3 +11,10 @@ encode ::    [String] ->           [(int, [String])]       ->  [String]
 
 --this was just as a reminder on the function syntax
 --doubleMe x = x + x
+
+
+
+
+
+--------------- ok. let's start with something simple. let's just see if we can parse the first part, what i've termed "a row". Actually,
+--------------- I belive that's supposed to be a list of lists of Strings, [[String]], so let's do that.
