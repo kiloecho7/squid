01f41b6e8250263251222dd604d088440d652430
Kevin Edwards <kevin.edwards@octanner.com>
2017-04-10 20:55:55 -0600

    first commit

---
+++ parse_data.hs
@@ -0,0 +1,12 @@
+--Cat = Left | Middle | Right
+
+
+
+{-
+              a row             index with categoricals
+          [100, 200, left]     (2, [left, middle, Right])  ->  [100, 200, 1, 0, 0]
+encode ::    [String] ->           [(int, [String])]       ->  [String]
+-}
+
+
+doubleMe x = x + x
