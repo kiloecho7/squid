8c4d18f761f321b622a9f2ba9313c40713853367
Kevin Edwards <kevin.edwards@octanner.com>
2017-04-26 23:11:57 -0600

    Take the working solution and refactor it -- partially applied function, let/in, rename vars, infix function

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,51 +21,31 @@ encodeRow encodings row =
     go indexedRow encodings acc = foldl (\acc rowElem -> acc ++ encodeElement rowElem encodings) acc indexedRow
 
 
+--TODO ++ is okay here because our lists are small but it's not performant for large lists. can we use foldr and : instead? 
+--renamed val to rowElemValue. val wasn't expressive enough.
+--changed "M.member idx mappedEncodings" to "idx `M.member` mappedEncodings". It reads better
+--pulled out the whole lambda, made a function that does that calculation *and* made a partially applied version of it.
 encodeElement :: (Int, String) -> M.Map Int [String] -> [String]
 encodeElement rowElem mappedEncodings
-  | M.member idx mappedEncodings = foldl (\acc encoding -> acc ++ [encodeBit val encoding]) [] encodings
-  | otherwise              = [val]
-  where idx = fst rowElem
-        val = snd rowElem
-        encodings = mappedEncodings M.! idx
+  | idx `M.member` mappedEncodings = foldl theFoldFunction' [] encodings
+  | otherwise                      = [val]
+  where idx              = fst rowElem
+        val              = snd rowElem
+        encodings        = mappedEncodings M.! idx
+        theFoldFunction' = theFoldFunction val
 
 
+--TODO can [String] be changed to [a]? IIRC the compiler will see that it's always a String that's passed in and it will complain. Maybe it's only a warning.
+--since there's only one function body are the types even needed? 
+--yay! we got to show off 'let'!
+theFoldFunction :: String -> [String] -> String -> [String]
+theFoldFunction rowElemValue acc encoding =
+  let encoded = encodeBit rowElemValue encoding
+  in acc ++ [encoded]
+
+
+--TODO There's *got* to be a much much more succinct way of doing this
 encodeBit :: String -> String -> String
 encodeBit value encoding
   | value == encoding = "1"
   | otherwise         = "0"
-
-
---map over every element in indexedRow... make a partial function with the encodings... [] is the initial accumulator.
-
-
---if there's an encoding, encode the row value. if there's not, return the row value.
-
-{-
-encodings = M.fromList [(0, ["left", "right", "middle"])]
-inputRow = ["left"]
-expectedRow = ["1", "0", "0"]
--}
-
-{-
-encodings = M.fromList [(0, ["left", "right", "middle"])]
-inputRow = ["left", "apple"]
-expectedRow = ["1", "0", "0", "apple"]
--}
-
-{-
-encodings = M.fromList [ (0, ["left", "middle", "right"])
-                       , (2, ["left", "middle", "right"])
-                       ]
-
-inputRow    = [     "left",      "100",      "middle",        "100", "200"]
-expectedRow = ["1", "0", "0",    "100",    "0", "1", "0",     "100", "200"]
--}
-
-{-
-    go encodings indexedRow = ["1", "0", "0"]
-    go :: M.Map Int [String]
-       -> M.Map Int [String]
-       -> [String]
-    go encodings indexedRow = ["1", "0", "0"]
--}
